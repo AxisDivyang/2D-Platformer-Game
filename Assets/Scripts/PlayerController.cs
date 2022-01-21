56febce8df02812f9847afe24656523c9af28b9f
Divyang Parekh <axis.divyang@gmail.com>
2022-01-21 16:23:13 +0530

    Scripting Animation & Flipping
    [**Task 1] - Add code to accept horizontal input from the user.**
    1. Binding Animations with Inputs
    2. Flipping Character
    
    **[Task 2] - Add code to play run animation and flip player according to the horizontal input.**
    1. Binding Animations with Inputs
    2. Flipping Character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("My Script is Work.");
+        //WHen I am Pressing Left or Right or Joy Stick Key when Movement that called.
+        float speed = Input.GetAxisRaw("Horizontal"); // Out Put will Be -1 or +1
+        animator.SetFloat("Speed", Mathf.Abs(speed) + 1);
+        Vector3 scale = transform.localScale;
+       
+        if ( speed < 0)   // Left Side
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+            Debug.Log("Speed ("+ speed  + ") < 0 :  Scale X : " + scale.x);
+        }
+        else if (speed > 0)  // Right Side
+        {
+            scale.x = Mathf.Abs(scale.x);
+            Debug.Log("Speed (" + speed + ") > 0 Scale X : " + scale.x);
+        }
+
+        transform.localScale = scale;
     }
 }
