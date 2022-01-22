7626c8845c4f035778a82906db01a99c322bd885
Divyang Parekh <axis.divyang@gmail.com>
2022-01-22 14:29:15 +0530

    Crouch Animation
    Make crouch animation and on pressing Cntrl key on keyboard player crouches and we also resize the collider.
    
    Accept the vertical in the same way as we accepted the horizontal input and when vertical > 0 then play jump animation.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,24 +11,89 @@ public class PlayerController : MonoBehaviour
         
     }
 
+
     // Update is called once per frame
     void Update()
     {
+
         //WHen I am Pressing Left or Right or Joy Stick Key when Movement that called.
         float speed = Input.GetAxisRaw("Horizontal"); // Out Put will Be -1 or +1
+        float local_speed = animator.GetFloat("Speed");
         animator.SetFloat("Speed", Mathf.Abs(speed) + 1);
         Vector3 scale = transform.localScale;
-       
-        if ( speed < 0)   // Left Side
+
+        float vertical_data = Input.GetAxisRaw("Vertical"); // Out Put will Be -1 or +1
+         
+        //if (speed < 0)   // Left Side
+        //{
+        //    animator.SetBool("Crouch", false);
+        //    scale.x = -1f * Mathf.Abs(scale.x);
+        //    Debug.Log("Speed (" + speed + ") < 0 :  Scale X : " + scale.x);
+        //}
+        //else if (speed > 0)  // Right Side
+        //{
+        //    animator.SetBool("Crouch", false);
+        //    scale.x = Mathf.Abs(scale.x);
+        //    Debug.Log("Speed (" + speed + ") > 0 Scale X : " + scale.x);
+        //}
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) || (Input.GetKeyDown(KeyCode.RightControl) )) //CTRL Control
         {
-            scale.x = -1f * Mathf.Abs(scale.x);
-            Debug.Log("Speed ("+ speed  + ") < 0 :  Scale X : " + scale.x);
+            //Debug.Log("Local Speed (" + local_speed + ") ");
+            animator.SetFloat("Speed", 1.5f);
+            animator.SetBool("Crouch", true);
+            animator.SetBool("Walk", false);
+            animator.SetBool("Jump", false);
+            //Debug.Log("Local Speed (" + local_speed + ") ");
         }
-        else if (speed > 0)  // Right Side
+
+        if (Input.GetKeyDown(KeyCode.F) ) //F = Walk Control
+        {
+            animator.SetFloat("Speed", 1.5f);
+            animator.SetBool("Crouch", false);
+            animator.SetBool("Walk", true);
+            animator.SetBool("Jump", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.J)) //J = Jump Control
+        {
+            animator.SetFloat("Speed", 1.5f);
+            animator.SetBool("Crouch", false);
+            animator.SetBool("Walk", false);
+            animator.SetBool("Jump", true);
+        }
+
+        if (speed > 0) //Right Side
         {
             scale.x = Mathf.Abs(scale.x);
-            Debug.Log("Speed (" + speed + ") > 0 Scale X : " + scale.x);
+            animator.SetBool("Crouch", false);
+            animator.SetBool("Walk", false);
+            animator.SetBool("Jump", false);
         }
+        if (speed < 0)  // Left Side
+        {
+            animator.SetBool("Crouch", false);
+            animator.SetBool("Walk", false);
+            animator.SetBool("Jump", false);
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+
+        if(vertical_data > 0)
+        {
+            animator.SetFloat("Speed", 1.5f);
+            animator.SetBool("Jump", true);
+            animator.SetBool("Crouch", false);
+            animator.SetBool("Walk", false);
+        }
+
+        if (vertical_data < 0)
+        {
+            animator.SetFloat("Speed", 1.5f );
+            animator.SetBool("Jump", false);
+            animator.SetBool("Crouch", true);
+            animator.SetBool("Walk", false);
+        }
+
 
         transform.localScale = scale;
     }
