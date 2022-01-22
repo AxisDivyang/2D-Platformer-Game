fe44160926b0ce32242da07eeb9c4c70c1eba207
Divyang Parekh <axis.divyang@gmail.com>
2022-01-22 19:41:40 +0530

    Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,96 +5,89 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public float speed;
+    public float jump;
+    private Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
     }
-
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal    = Input.GetAxisRaw("Horizontal"); // Out Put will Be -1 or +1
+        float vertical      = Input.GetAxisRaw("Jump"); // Out Put will Be -1 or +1
+        PlayMovementAnimation( horizontal, vertical );
+        MoveCharacterControl( horizontal, vertical );
+    }
 
+    private void MoveCharacterControl( float horizontal , float vertical )
+    {
+        //Move Character Horizontally 
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+
+        //Move Character Vertically
+        if(vertical > 0)
+        {
+            rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+        }
+        
+    }
+
+    private void PlayMovementAnimation( float horizontal, float vertical )
+    {
         //WHen I am Pressing Left or Right or Joy Stick Key when Movement that called.
-        float speed = Input.GetAxisRaw("Horizontal"); // Out Put will Be -1 or +1
-        float local_speed = animator.GetFloat("Speed");
-        animator.SetFloat("Speed", Mathf.Abs(speed) + 1);
         Vector3 scale = transform.localScale;
+        animator.SetFloat("Speed", Mathf.Abs(horizontal) );
 
-        float vertical_data = Input.GetAxisRaw("Vertical"); // Out Put will Be -1 or +1
-         
-        //if (speed < 0)   // Left Side
-        //{
-        //    animator.SetBool("Crouch", false);
-        //    scale.x = -1f * Mathf.Abs(scale.x);
-        //    Debug.Log("Speed (" + speed + ") < 0 :  Scale X : " + scale.x);
-        //}
-        //else if (speed > 0)  // Right Side
-        //{
-        //    animator.SetBool("Crouch", false);
-        //    scale.x = Mathf.Abs(scale.x);
-        //    Debug.Log("Speed (" + speed + ") > 0 Scale X : " + scale.x);
-        //}
-
-        if (Input.GetKeyDown(KeyCode.LeftControl) || (Input.GetKeyDown(KeyCode.RightControl) )) //CTRL Control
-        {
-            //Debug.Log("Local Speed (" + local_speed + ") ");
-            animator.SetFloat("Speed", 1.5f);
-            animator.SetBool("Crouch", true);
-            animator.SetBool("Walk", false);
-            animator.SetBool("Jump", false);
-            //Debug.Log("Local Speed (" + local_speed + ") ");
-        }
-
-        if (Input.GetKeyDown(KeyCode.F) ) //F = Walk Control
-        {
-            animator.SetFloat("Speed", 1.5f);
-            animator.SetBool("Crouch", false);
-            animator.SetBool("Walk", true);
-            animator.SetBool("Jump", false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.J)) //J = Jump Control
-        {
-            animator.SetFloat("Speed", 1.5f);
-            animator.SetBool("Crouch", false);
-            animator.SetBool("Walk", false);
-            animator.SetBool("Jump", true);
-        }
-
-        if (speed > 0) //Right Side
+        if (horizontal > 0) //Right Side
         {
             scale.x = Mathf.Abs(scale.x);
             animator.SetBool("Crouch", false);
             animator.SetBool("Walk", false);
             animator.SetBool("Jump", false);
         }
-        if (speed < 0)  // Left Side
+        else if (horizontal < 0)  // Left Side
         {
             animator.SetBool("Crouch", false);
             animator.SetBool("Walk", false);
             animator.SetBool("Jump", false);
             scale.x = -1f * Mathf.Abs(scale.x);
         }
+        transform.localScale = scale;
 
-        if(vertical_data > 0)
-        {
-            animator.SetFloat("Speed", 1.5f);
-            animator.SetBool("Jump", true);
-            animator.SetBool("Crouch", false);
-            animator.SetBool("Walk", false);
-        }
 
-        if (vertical_data < 0)
+        if (Input.GetKeyDown(KeyCode.LeftControl) || (Input.GetKeyDown(KeyCode.RightControl))) //CTRL Control
         {
-            animator.SetFloat("Speed", 1.5f );
-            animator.SetBool("Jump", false);
+            //Debug.Log("Local Speed (" + local_speed + ") ");
             animator.SetBool("Crouch", true);
             animator.SetBool("Walk", false);
+            animator.SetBool("Jump", false);
+            //Debug.Log("Local Speed (" + local_speed + ") ");
         }
 
+        //if (Input.GetKeyDown(KeyCode.F)) //F = Walk Control
+        //{
+        //    animator.SetFloat("Speed", 1.5f);
+        //    animator.SetBool("Crouch", false);
+        //    animator.SetBool("Walk", true);
+        //    animator.SetBool("Jump", false);
+        //}
 
-        transform.localScale = scale;
+        if (vertical > 0)
+        {
+            animator.SetBool("Jump", true);
+             
+        }
+        else if (vertical < 0)
+        {
+            animator.SetBool("Jump", false);
+             
+        }
     }
 }
