153cd70f25e3050f31089acf7cae68a91e7e11be
AxisDivyang <axis.divyang@gmail.com>
2022-01-31 13:04:44 +0530

    Level Complete and Load Next Level

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if( collision.gameObject.GetComponent<PlayerController>() != null )
+        {
+            collision.gameObject.GetComponent<LoadScenesController>().LoadNextLevel();
+            Debug.Log("Level Completed..Move to next level");
+        } 
     }
 }
---
+++ Assets/Scripts/LoadScenesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenesController : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1 );
+        //SceneManager.LoadSceneAsync("Level_1");
+    }
+}
